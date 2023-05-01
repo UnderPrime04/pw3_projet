51ed5cb9292637fdd213addd73870ff4f455bba0
gustavo <gustavo.gumesi@gmail.com>
2023-05-01 18:21:27 -0300

    Subindo projet

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pw3_atvdP';
+
+  public logo ="../assets/Design sem nome.png";
+  public logi ="../assets/logo.png";
+  public design ="../assets/opala-removebg-preview.png";
+  public twitter ="../assets/Twitter.png";
+  public insta ="../assets/Instagram.png";
+  public trofeu ="../assets/trofeu.png";
+  public bola ="../assets/bola.png";
+  public youtube ="../assets/Youtube.png";
+}
+
