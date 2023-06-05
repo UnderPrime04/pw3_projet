6d5ed96d8bba3722a5cf1056d237f54737024ed5
GUSTAVO <gustavo.gumesi@gmail.com>
2023-06-04 23:15:23 -0300

    Subindo WEB TCC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw3_atvdP';
+  title = 'OPALA_PW';
 
-  public logo ="../assets/Design sem nome.png";
-  public logi ="../assets/logo.png";
-  public design ="../assets/opala-removebg-preview.png";
-  public twitter ="../assets/Twitter.png";
-  public insta ="../assets/Instagram.png";
-  public trofeu ="../assets/trofeu.png";
-  public bola ="../assets/bola.png";
-  public youtube ="../assets/Youtube.png";
+  public logo ="../assets/opala.png";
 }
 
